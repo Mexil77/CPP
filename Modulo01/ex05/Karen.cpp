dcddb528214284e81452ec2db33a399946739500
Emmanuel Garcia torres <emgarcia@c3r6s5.42madrid.com>
2022-02-25 17:20:56 +0100

    Agregando cosas

---
+++ Modulo01/ex05/Karen.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/25 16:00:29 by emgarcia          #+#    #+#             */
+/*   Updated: 2022/02/25 16:20:36 by emgarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+Karen::Karen(void)
+{
+	std::cout << "An annoying Karen has born" << std::endl;
+}
+
+Karen::~Karen(void)
+{
+	std::cout << "Karen finaly has been destroyed" << std::endl;
+}
+
+void	Karen::debug(void)
+{
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!." << std::endl;
+}
+
+void	Karen::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+}
+
+void	Karen::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+}
+
+void	Karen::error(void)
+{
+	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+void	Karen::complain(std::string level)
+{
+	void	(*funptr)(void);
+
+	funptr = this->debug;
+	std::cout << "level " << level << std::endl;
+	this->debug();
+}
---
+++ Modulo01/ex05/Karen.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/25 16:00:39 by emgarcia          #+#    #+#             */
+/*   Updated: 2022/02/25 16:01:06 by emgarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KAREN_HPP
+# define KAREN_HPP
+
+# include <string>
+# include <iostream>
+
+class	Karen
+{
+	public:
+		Karen(void);
+		~Karen(void);
+		void	complain(std::string level);
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+};
+
+#endif

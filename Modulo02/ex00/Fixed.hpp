2618993191f03ddb9fab332b983167dc205f5efa
Emmanuel Garcia torres <emgarcia@c3r12s3.42madrid.com>
2022-03-02 12:44:53 +0100

    Agregando el 8 como numero y no como caracter

--- Modulo02/ex00/Fixed.hpp
+++ Modulo02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 19:14:56 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/03/01 18:08:22 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/03/02 12:44:01 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ class	Fixed
 		~Fixed(void);
 
 	private:
-		static const int	_Q = '8';
+		static const int	_Q = 8;
 		
 		int	_Z;
 };

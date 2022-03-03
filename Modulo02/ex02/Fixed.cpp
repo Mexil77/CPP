5e341ff1cdf99d9f6ebe232034c2ad6153500751
Emmanuel Garcia torres <emgarcia@c3r4s6.42madrid.com>
2022-03-03 15:36:39 +0100

    empezando M02ex02

---
+++ Modulo02/ex02/Fixed.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 15:33:07 by emgarcia          #+#    #+#             */
+/*   Updated: 2022/03/03 15:33:25 by emgarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called." << std::endl;
+	this->_Z = 0;
+}
+
+Fixed::Fixed(const Fixed& obj)
+{
+	std::cout << "Copy constructor called." << std::endl;
+	this->operator=(obj);
+}
+
+Fixed::Fixed(const float Q)
+{
+	std::cout << "Float constructor called." << std::endl;
+	this->_Z = round(Q * (1 << this->_Q));
+}
+
+Fixed::Fixed(const int Z)
+{
+	std::cout << "Int constructor called." << std::endl;
+	this->_Z = round(Z * (1 << this->_Q));
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called." << std::endl;
+}
+
+Fixed&	Fixed::operator=(Fixed const &fix)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->setRawBits(fix.getRawBits());
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	return (this->_Z);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_Z = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)this->_Z / (1 << this->_Q));
+}
+
+int	Fixed::toInt(void) const
+{
+	return ((int)this->_Z / (1 << this->_Q));
+}
+
+std::ostream&	operator<<(std::ostream &o, Fixed const &fix)
+{
+	o << fix.toFloat();
+	return (o);
+}
---
+++ Modulo02/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 15:34:06 by emgarcia          #+#    #+#             */
+/*   Updated: 2022/03/03 15:35:18 by emgarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}

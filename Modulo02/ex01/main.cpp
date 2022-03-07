cfb237f3b2ce434493b42faa33472a0ad7b8f148
Emmanuel Garcia torres <emgarcia@c2r10s4.42madrid.com>
2022-03-07 18:48:19 +0100

    empezando M03ex00

--- Modulo02/ex01/main.cpp
+++ Modulo02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 12:46:11 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/03/03 14:37:55 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/03/07 15:38:18 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,9 @@ int main(void)
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
 	return 0;
 }

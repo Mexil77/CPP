af225e9c2b4f44e59bf0e5134c3d936974c0fa34
Emmanuel Garcia torres <emgarcia@c1r6s6.42madrid.com>
2022-03-16 13:24:05 +0100

    terminando M04ex01

--- Modulo04/ex01/main.cpp
+++ Modulo04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 15:55:13 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/03/16 12:52:54 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/03/16 13:23:09 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,30 @@ int	main(void)
 	{
 		const Animal* i = new Cat();
 		const Animal* j = new Dog();
-		i->showIdeas();
-		j->showIdeas();
+		// i->showIdeas();
+		// j->showIdeas();
 		delete i;
 		delete j;
+		system("leaks brain");
+		std::cout << std::endl;
+	}
+	{
+		size_t	i;
+		Animal	*animals[100];
+
+		i = -1;
+		while (++i < 100)
+		{
+			if (i < 50)
+				animals[i] = new Dog();
+			else
+				animals[i] = new Cat();
+		}
+		animals[35]->makeSound();
+		animals[75]->makeSound();
+		i = -1;
+		while (++i < 100)
+			delete animals[i];
 		system("leaks brain");
 	}
     return 0;

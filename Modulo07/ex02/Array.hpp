febba395c71eda288c2951aa17e0067bc178bb83
Emmanuel Garcia torres <emgarcia@c3r6s4.42madrid.com>
2022-05-30 18:57:08 +0200

    Finish M07

--- Modulo07/ex02/Array.hpp
+++ Modulo07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 13:58:47 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/05/26 20:23:40 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/05/30 18:49:07 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,31 @@ class Array
 		~Array();
 
 		Array&	operator = (const Array &arr);
-		void*	operator new [] (size_t i);
-		// T&		operator [] (size_t i);
+		T&		operator[](int);
 		
 		size_t	getSize(void);
 		T*		getArr(void);
 		T		getArrData(size_t i);
+		void	setSize(size_t);
+
+		class InvalidIndexException: public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Invalid index");
+				}
+		};
+
 	private:
 		size_t	_size;
 		T		*_arr;
 };
 
 template<typename T>
-void*	Array<T>::operator new[] (size_t i)
-{
-	_size = i;
-	// std::cout << "An Array has been created with " << n << " elemnts." << std::endl;
-	for (size_t i = 0; i < _size; i++)
-		_arr[i] = 1;
-}
-
-template<typename T>
-Array<T>::Array(void) : _size(1), _arr(new T[1])
+Array<T>::Array(void) : _size(0), _arr(new T[0])
 {
 	std::cout << "An Array has been created with empty constructor." << std::endl;
-	_arr[0] = 0;
 }
 
 template<typename T>
@@ -66,38 +66,38 @@ Array<T>::Array(unsigned int n) : _size(static_cast<size_t>(n)), _arr(new T[n])
 {
 	std::cout << "An Array has been created with " << n << " elemnts." << std::endl;
 	for (size_t i = 0; i < _size; i++)
-		_arr[i] = 1;
+		_arr[i] = 0;
 }
 
 template<typename T>
 Array<T>::~Array(void)
 {
 	std::cout << "An Array has been Destroied." << std::endl;
-	delete _arr;
+	delete [] _arr;
 }
 
 template<typename T>
 Array<T>&	Array<T>::operator = (Array const &arr)
 {
-	delete _arr;
-	_size = arr._size;
-	_arr = new T[_size];
-	for (size_t i = 0; i < _size; i++)
-		_arr[i] = arr._arr[i];
-	std::cout << "An Array has been created with " << _size << " elemnts as a asignation." << std::endl;
+	if (this != &arr )
+	{
+		delete [] _arr;
+		_size = arr._size;
+		_arr = new T[_size];
+		for (size_t i = 0; i < _size; i++)
+			_arr[i] = arr._arr[i];
+		std::cout << "An Array has been created with " << _size << " elemnts as a asignation." << 	std::endl;
+	}
 	return (*this);
 }
 
-// template<typename T>
-// T&	Array<T>::operator[] (size_t i)
-// {
-// 	if (i >= _size)
-// 	{
-// 		std::cout << "Array index out of bound, exiting" << std::endl;
-// 		exit(0);
-// 	}
-// 	return (_arr[i]);
-// }
+template<typename T>
+T&	Array<T>::operator[] (int i)
+{
+	if (i >= static_cast<int>(_size) || i < 0)
+		throw Array<T>::InvalidIndexException();
+	return (_arr[i]);
+}
 
 template<typename T>
 size_t	Array<T>::getSize(void)
@@ -115,6 +115,12 @@ template<typename T>
 T	Array<T>::getArrData(size_t i)
 {
 	return (_arr[i]);
+}
+
+template<typename T>
+void	Array<T>::setSize(size_t i)
+{
+	this->_size = i;
 }
 
 template<typename T>
--- Modulo07/ex02/main.cpp
+++ Modulo07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 13:57:13 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/05/26 20:23:54 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/05/30 18:56:28 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,40 @@
 
 int main(void)
 {
-	Array<int>		*A = new Array<int>[5];
+	Array<int>		*A = new Array<int>(5);
 	Array<float>	*B = new Array<float>(8);
 	Array<int>		*C = new Array<int>(*A);
 	Array<float>	*D = new Array<float>();
-	// float			p = B[2];
-	// B[2] = 3;
+	Array<float>	*F = new Array<float>();
+
 	*D = *B;
+	std::cout << std::endl;
+	try
+	{
+		(*B)[0] = 5.2;
+		(*D)[6] = 13.627;
+		(*F)[8] = 8.8;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	std::cout << std::endl;
 	std::cout << "A: " << A << std::endl;
 	std::cout << "*A: " << *A << std::endl;
-	// std::cout << "A[0]: " << A[0] << std::endl;
 	std::cout << "B: " << B << std::endl;
 	std::cout << "*B: " << *B << std::endl;
 	std::cout << "C: " << C << std::endl;
 	std::cout << "*C: " << *C << std::endl;
 	std::cout << "D: " << D << std::endl;
 	std::cout << "*D: " << *D << std::endl;
+	std::cout << "F: " << F << std::endl;
+	std::cout << "*F: " << *F << std::endl;
 	delete A;
 	delete B;
 	delete C;
 	delete D;
+	delete F;
 	// system("leaks Array");
 	return 0;
 }

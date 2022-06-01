dfeb7fbe52a20dfce7825be5dba6d512ef3b5e0c
Emmanuel Garcia torres <emgarcia@c3r6s1.42madrid.com>
2022-06-01 16:17:39 +0200

    Finish M08ex00

--- Modulo08/ex00/easyfind.hpp
+++ Modulo08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 15:58:44 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/05/31 17:38:53 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/06/01 16:15:48 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,23 @@
 
 # include <string>
 # include <iostream>
-# include <vector>
-# include <list>
+
+class NotFoundException: public std::exception
+{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("Number not found");
+		}
+};
 
 template<typename T>
-bool	easyfind(T V, int find)
+typename T::iterator	easyfind(T V, int find)
 {
-	for (size_t i = 0; i < V.size(); i++)
-		if (find == V[i])
-			return (true);
-	return (false);
+	for (typename T::iterator i = V.begin(); i != V.end(); i++)
+		if (*i == find)
+			return (i);
+	throw NotFoundException();
 }
 
 #endif
--- Modulo08/ex00/main.cpp
+++ Modulo08/ex00/main.cpp
@@ -6,40 +6,45 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 15:57:53 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/05/31 17:50:58 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/06/01 16:15:45 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <vector>
+#include <list>
 
 int main(void)
 {
-	int							find = 30;
-	std::vector<int>			V;
-	std::list<int>				L;
-	std::vector<int>::iterator	b;
+	int					find = 3;
+	std::vector<int>	V;
+	std::list<int>		L;
 	V.push_back(30);
 	V.push_back(3);
-	// V.push_back(266);
-	// V.push_back(456);
+	V.push_back(266);
+	V.push_back(456);
 	L.push_back(2);
 	L.push_back(24);
 	L.push_back(249);
 	L.push_back(2494);
-	b = V.begin();
-	std::cout << "b iterator: " << *b++ << std::endl;
-	std::cout << "b iterator: " << *b++ << std::endl;
-	std::cout << "b iterator: " << *b << std::endl;
-	std::cout << "V.size: " << V.size() << std::endl;
-	for (size_t i = 0; i < V.size(); i++)
-		std::cout << "V[ " << i << " ]: " << V[i] << std::endl;
-	if (easyfind(V, find))
-		std::cout << "El numero " << find << " SI esta en el vector." << std::endl;
-	else
-		std::cout << "El numero " << find << " NO esta en el vector." << std::endl;
-	// if (easyfind(L, find))
-	// 	std::cout << "El numero " << find << " SI esta en la lista." << std::endl;
-	// else
-	// 	std::cout << "El numero " << find << " NO esta en la lista." << std::endl;
+	L.push_back(30);
+	try
+	{
+		std::vector<int>::iterator v = easyfind(V, find);
+		std::cout << "El numero " << *v << " SI esta en el vector." << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::list<int>::iterator l = easyfind(L, find);
+		std::cout << "El numero " << *l << " SI esta en la lista." << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }

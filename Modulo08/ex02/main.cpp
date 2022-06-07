7b94531e8d52b25ef403e42e3a10ba1cebb04c67
Emmanuel Garcia torres <emgarcia@c3r13s1.42madrid.com>
2022-06-07 17:59:01 +0200

    fixing things

--- Modulo08/ex02/main.cpp
+++ Modulo08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 12:47:05 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/06/06 17:26:40 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/06/07 17:11:10 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,25 @@
 int main(void)
 {
 	MutantStack<int>	a;
-	MutantStack<int>	b(a);
-	MutantStack<int>	c;
-	MutantStack<int>	e;
-
 	a.push(5);
-	a.push(10);
-	MutantStack<int>	d(a);
-	c = b;
-	e = d;
-	a.pop();
+	std::cout << "a.size: " << a.size() << std::endl;
+	std::cout << "a.top: " << a.top() << std::endl;
+	// MutantStack<int>	b(a);
+	// MutantStack<int>	c;
+	// MutantStack<int>	e;
 
-	std::cout << std::endl;
-	std::cout << "a :" << std::endl << a << std::endl;
-	std::cout << "b :" << std::endl << b << std::endl;
-	std::cout << "c :" << std::endl << c << std::endl;
-	std::cout << "d :" << std::endl << d << std::endl;
-	std::cout << "e :" << std::endl << e << std::endl;
+	// a.push(10);
+	// MutantStack<int>	d(a);
+	// c = b;
+	// e = d;
+	// a.pop();
+
+	// std::cout << std::endl;
+	// std::cout << "a :" << std::endl << a << std::endl;
+	// std::cout << "b :" << std::endl << b << std::endl;
+	// std::cout << "c :" << std::endl << c << std::endl;
+	// std::cout << "d :" << std::endl << d << std::endl;
+	// std::cout << "e :" << std::endl << e << std::endl;
 	// try
 	// {
 	// 	std::cout << "a.top: " << a.top() << std::endl;
--- Modulo08/ex02/mutantstack.hpp
+++ Modulo08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 12:49:01 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/06/06 19:26:38 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/06/07 17:58:24 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,137 +18,16 @@
 # include <stack>
 
 template <typename T>
-class MutantStack
+class MutantStack: public std::stack<T>
 {
 	public:
-		MutantStack(void);
-		MutantStack(const MutantStack &obj);
-		~MutantStack();
-
-		MutantStack<T>&	operator = (MutantStack &mut);
-
-		std::stack<T>&	getStack(void);
-		size_t			size(void);
-		void			push(T n);
-		T&				top(void);
-		void			pop(void);
-		bool			empty(void);
-
-		class iterator
-		{
-			public:
-				iterator(void);
-				~iterator();
-			private:
-				T	*_it;
-				std::stack<T>	_itStack;
-		};
-
-		class EmptyException: public std::exception
-		{
-			public:
-				virtual const char* what() const throw()
-				{
-					return ("Empty MutantStack.");
-				}
-		};
-	private:
-		std::stack<T>	_stack;
+		typedef typename MutantStack<T>::container_type::iterator iterator;
+		iterator	beguin(void) const;
 };
 
-// MutatnSack::Iterator
-
 template<typename T>
-MutantStack<T>::iterator::iterator(void): _it(new T(0)), _itStack(new std::stack)
+iterator	MutantStack<T>::beguin(void)
 {
-	std::cout << "A MutantSack Iterator has been created." << std::endl;
-}
-
-// MutantStack
-
-template<typename T>
-MutantStack<T>::MutantStack(void)
-{
-	std::cout << "A MutantSack has been created." << std::endl;
-}
-
-template<typename T>
-MutantStack<T>::MutantStack(const MutantStack &obj)
-{
-	if (!obj._stack.empty())
-	{
-		std::stack<T>	tmp = obj._stack;
-		while (!tmp.empty())
-		{
-			_stack.push(tmp.top());
-			tmp.pop();
-		}
-	}
-	std::cout << "A MutantSack has been created as a copy." << std::endl;
-}
-
-template<typename T>
-MutantStack<T>::~MutantStack()
-{
-	std::cout << "A MutantSack has been deleted." << std::endl;
-}
-
-template<typename T>
-MutantStack<T>&	MutantStack<T>::operator = (MutantStack &mut)
-{
-	if (!mut._stack.empty())
-	{
-		std::stack<T>	tmp = mut._stack;
-		while (!tmp.empty())
-		{
-			_stack.push(tmp.top());
-			tmp.pop();
-		}
-	}
-	std::cout << "A MutantSack has been created as an asignation." << std::endl;
-	return (*this);
-}
-
-template<typename T>
-std::stack<T>&	MutantStack<T>::getStack(void) {return (_stack);}
-
-template<typename T>
-size_t	MutantStack<T>::size(void) {return (_stack.size());}
-
-template<typename T>
-void	MutantStack<T>::push(T n){_stack.push(n);}
-
-template<typename T>
-T&	MutantStack<T>::top(void)
-{
-	if (_stack.empty())
-		throw MutantStack<T>::EmptyException();
-	return (_stack.top());
-}
-
-template<typename T>
-void	MutantStack<T>::pop(void)
-{
-	if (_stack.empty())
-		throw MutantStack<T>::EmptyException();
-	_stack.pop();
-}
-
-template<typename T>
-bool	MutantStack<T>::empty() {return (_stack.empty());}
-
-template<typename T>
-std::ostream&	operator << (std::ostream &o, MutantStack<T> &mut)
-{
-	if (mut.empty())
-		o << "Empty MutantStack" << std::endl;
-	std::stack<T>	tmp = mut.getStack();
-	while (!tmp.empty())
-	{
-		o << "stack [ " << tmp.top() << " ]" << std::endl;
-		tmp.pop();
-	}
-	return (o);
 }
 
 #endif

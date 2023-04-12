f724eabb5a79f400b5ce5d1fc86d37c96bbaaf0f
Mexil77 <mendel_77@hotmail.com>
2023-04-12 19:15:51 +0200

    data in map container M09ex00

--- Modulo09/ex00/main.cpp
+++ Modulo09/ex00/main.cpp
@@ -1,19 +1,49 @@
 #include "BitcoinExchange.hpp"
+#include <fstream>
+#include <bits/stdc++.h>
 
-int	main(void)
-{
-	BitcoinExchange bitExch;
-	bitExch.dataMap["c"] = "1";
-	bitExch.dataMap["b"] = "2";
-	bitExch.dataMap["a"] = "3";
+void	printMap(std::map<std::string, std::string> m){
 
-	BitcoinExchange bitExchCopi(bitExch);
 	std::cout << "{" << std::endl;
 	std::map<std::string, std::string>::iterator iter;
-	for (iter = bitExchCopi.dataMap.begin(); iter != bitExchCopi.dataMap.end(); iter++)
+	for (iter = m.begin(); iter != m.end(); iter++)
 	{
 		std::cout	<< iter->first << ':' << iter->second << "," << std::endl;
 	}
 	std::cout << "}" << std::endl;
+}
+
+void	fillMap(std::map<std::string, std::string> &m, std::fstream *fdin) {
+	std::string	cadena;
+	std::string	str;
+	int					i;
+
+	while (!fdin->eof())
+	{
+		*fdin >> cadena;
+		std::stringstream	ss(cadena);
+		std::string				key;
+		std::string				value;
+		i = 0;
+		while (getline(ss, str, ','))
+			i++ % 2 == 0 ? key = str : value = str;
+		m[key] = value;
+	}
+}
+
+int	main(void)
+{
+	std::fstream		fdin;
+	BitcoinExchange	bitExch;
+
+	fdin.open("data.csv", std::fstream::in);
+	if (!fdin)
+	{
+		std::cout << "Error de lectura" << std::endl;
+		return(0);
+	}
+	fillMap(bitExch.dataMap, &fdin);
+	fdin.close();
+	printMap(bitExch.dataMap);
 	return 0;
 }

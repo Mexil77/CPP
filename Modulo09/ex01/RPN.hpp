cac949a9078787780f9233470e7b5306fd82b245
Mexil77 <mendel_77@hotmail.com>
2024-01-21 22:17:11 +0100

    finish Moduele09ex01

---
+++ Modulo09/ex01/RPN.hpp
@@ -0,0 +1,21 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <iostream>
+# include <stack>
+# include <string>
+
+class RPN
+{
+	public:
+		~RPN() {};
+		RPN& operator=(const RPN& other);
+		
+		static void doRPN(char *str);
+	private:
+		RPN(RPN& copy);
+		RPN() {};
+
+};
+
+#endif
